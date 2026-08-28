f9269032e04ed9d06ca499e93265ee0d83c5749e
versity007 <versity@gmail.com>
2026-08-28 12:51:47 -0500

    Update image imports in loop-diagram.tsx

--- src/components/loop-diagram.tsx
+++ src/components/loop-diagram.tsx
@@ -1,6 +1,6 @@
-import grievanceImg from "@/assets/12-2-panel2-release-portrait.png";
-import releaseImg from "@/assets/13-2-3-panel3-productive-portrait.png";
-import empoweredImg from "@/assets/1-3-1-panel1-grievance-portrait.png";
+import grievanceImg from "@/assets/2-2-panel2-release-portrait.png";
+import releaseImg from "@/assets/5-2-3-panel3-productive-portrait.png";
+import empoweredImg from "@/assets/4-3-1-panel1-grievance-portrait.png";
 
 const PANELS = [
   {
