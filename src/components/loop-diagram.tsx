0306adac9ae96157a59a7fdd26eb4deb4702d3b6
versity007 <versity@gmail.com>
2026-08-27 21:38:02 -0500

    Update release image path in loop-diagram.tsx

--- src/components/loop-diagram.tsx
+++ src/components/loop-diagram.tsx
@@ -1,5 +1,5 @@
 import grievanceImg from "@/assets/2-panel2-release-portrait.png";
-import releaseImg from "@/assets/5-2-3-panel3-productive-portrait.png";
+import releaseImg from "@/assets/7-2-3-panel3-productive-portrait.png";
 import productiveImg from "@/assets/6-3-1-panel1-grievance-portrait.png";
 
 const PANELS = [
